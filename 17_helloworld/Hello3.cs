9733a32da97663c7e54eb27c3b938619e14d1c80
blueyi <blueyiniu@qq.com>
2015-09-14 08:57:12 +0800

    update

---
+++ 17_helloworld/Hello3.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Hello3
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(args[0]);
+    }
+}
---
+++ 17_helloworld/hello2.cs
@@ -0,0 +1,8 @@
+using System;
+public class Hello2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
